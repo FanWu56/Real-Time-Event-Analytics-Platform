0dd63afd2ca3851b88f3ef6e351c730a5805e0a7
FanWu56 <za110q@proton.me>
2026-06-14 15:49:17 -0700

    Add files via upload

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,11 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import http from "http";
+import eventRoutes from "./routes/eventRoutes";
+import projectRoutes from "./routes/projectRoutes";
+import analyticsRoutes from "./routes/analyticsRoutes";
+import { initSocketServer } from "./websocket/liveSocket";
 
 // Read env
 dotenv.config()                      
@@ -18,8 +23,14 @@ app.get("/health", (req, res) => {
     })
 })
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on http://localhost:${PORT}`)
-})
+app.use("/api/events", eventRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/projects", analyticsRoutes);
+const httpServer = http.createServer(app);
 
+initSocketServer(httpServer);
+
+httpServer.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+})
 
